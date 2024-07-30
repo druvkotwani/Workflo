b5635df017566a6b749a6dc0d3ad32639c5f5844
druvkotwani <druvkotwani12@gmail.com>
2024-07-30 19:11:34 +0530

    mongodb server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 app.use(express.json())
 
-// mongodb+srv://hackerdruv:1UXkGzGuWZRBg28Z@workflo.skbz2gj.mongodb.net/
+function vaildEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
 
-// 1UXkGzGuWZRBg28Z
-
-// mongodb+srv://hackerdruv:1UXkGzGuWZRBg28Z@workflo.skbz2gj.mongodb.net/workflo?retryWrites=true&w=majority&appName=workflo
 
 app.use(cors())
 require('./db/connection')
@@ -15,8 +17,12 @@ const Users = require('./Models/User')
 
 app.post('/signup', async (req, res) => {
 
+    if (!vaildEmail(req.body.email)) {
+        return res.status(400).send({ message: '👻 Invalid email' })
+    }
+
     if (!req.body.name || !req.body.email || !req.body.password) {
-        return res.status(400).send({ message: 'Name, email and password are required' })
+        return res.status(400).send({ message: '👻 Name, email and password are required' })
     }
 
     const { email } = req.body
@@ -26,16 +32,45 @@ app.post('/signup', async (req, res) => {
     )
 
     if (existingUser) {
-        return res.status(400).send({ message: 'User with this email already exists' })
+        return res.status(400).send({ message: '😅 User with this email already exists' })
     }
-
-
-    let user = new Users(req.body)
+    let user = new Users({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 8)
+    })
     let result = await user.save()
     res.send(result)
-
-
-
 })
+
+// Sign-in endpoint
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await Users.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: '🥷 Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: ' 🤡Invalid email or password' });
+        }
+
+        // Generate a token (optional)
+        const token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+            expiresIn: '1h',
+        });
+
+        res.status(200).json({ message: 'Sign-in successful', token, name: user.name });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
 
 app.listen(8000)
